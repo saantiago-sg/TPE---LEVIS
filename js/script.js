27e496144bd7f578e888e79ce5d6de252c936a30
saantiago-sg <54604815+saantiago-sg@users.noreply.github.com>
2020-12-02 17:15:34 -0300

    Se agregaron archivos a la tabla y botones (eliminar y editar)
    El unico boton que tiene la funcion andando es eliminar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let productos = [{
+let productos = [
+    {
         nombreProd: "Remera",
         genero: "Hombre",
         talle: "M",
@@ -36,57 +37,52 @@ let productos = [{
     },
 ];
 
-// window.addEventListener("load", () => {
-//     for (let i = 0; i < productos.length; i++) {
-//         addTable(productos[i]);
-//     }
-// });
+window.addEventListener("load", () => {
+    actualizar();
+});
 
-// let table = document.querySelector("#table");
-// let addTable = (productos) => {
 
-//     let tbody = document.createElement("tbody");
-//     for(key in productos) {
-//         let fila = document.createElement("tr");
-//         let celda = document.createElement("td");
-//         celda.innerHTML = `${key} : ${productos[key]}`; 
-//         fila.appendChild(celda);
-//         tbody.appendChild(fila);
-//     }
-// table.appendChild(tbody);
-// }
 const cuerpo = document.querySelector("#cuerpo");
+let actualizar = () => { 
+    cuerpo.innerHTML = "";
 
-productos.forEach(producto => {
+    productos.forEach((producto, index) => {
+        const tr = document.createElement("tr");    //declaro constante tr  /fila/
+        tr.classList.add("bg-tr");
 
-    const tr = document.createElement("tr");    //declaro constante tr  /fila/
-    tr.classList.add("bg-tr");
+        let tdNombre = document.createElement("td");    // nombre de producto   /contenido de fila/
+        tdNombre.textContent = producto.nombreProd;
+        tr.appendChild(tdNombre);
+        
+        let tdGenero = document.createElement("td");    // genero
+        tdGenero.textContent = producto.genero;
+        tr.appendChild(tdGenero);
 
-    let tdNombre = document.createElement("td");    // nombre de producto   /contenido de fila/
-    tdNombre.textContent = producto.nombreProd;
-    tr.appendChild(tdNombre);
-    
-    let tdGenero = document.createElement("td");    // genero
-    tdGenero.textContent = producto.genero;
-    tr.appendChild(tdGenero);
+        let tdTalle = document.createElement("td");    // talle
+        tdTalle.textContent = producto.talle;
+        tr.appendChild(tdTalle);
 
-    let tdTalle = document.createElement("td");    // talle
-    tdTalle.textContent = producto.talle;
-    tr.appendChild(tdTalle);
+        let tdPrecio = document.createElement("td");    // precio
+        tdPrecio.textContent = producto.precio;
+        tr.appendChild(tdPrecio);
 
-    let tdPrecio = document.createElement("td");    // precio
-    tdPrecio.textContent = producto.precio;
-    tr.appendChild(tdPrecio);
+        let editar = document.createElement("button");
+        editar.innerHTML = "Editar";
+        editar.classList.add("btn-editar");
+        tr.appendChild(editar);
 
-    let editar = document.createElement("button");
-    editar.innerHTML = "Editar";
-    editar.classList.add("btn-editar");
-    tr.appendChild(editar);
+        let eliminar = document.createElement("button");
+        eliminar.innerHTML = "Borrar";
+        eliminar.classList.add("btn-borrar");
+        eliminar.addEventListener('click', () => eliminarProducto(index));
+        tr.appendChild(eliminar);
 
-    let eliminar = document.createElement("button");
-    eliminar.innerHTML = "Borrar";
-    eliminar.classList.add("btn-borrar");
-    tr.appendChild(eliminar);
+        cuerpo.appendChild(tr);
+    })  
+}
 
-    cuerpo.appendChild(tr);
-})
+let eliminarProducto = index =>{    //recibo index por parametro
+   productos = productos.filter((productos, i) => i !== index);  // comparo que el index sea distinto que i
+   actualizar();
+}
+
